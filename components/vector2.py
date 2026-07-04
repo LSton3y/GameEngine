0f1571d63e4f7306e771c561844741cb3ea43013
LSton3y <leolstone@icloud.com>
2026-07-04 09:59:43 +0100

    Added type hinting

--- components/vector2.py
+++ components/vector2.py
@@ -1,12 +1,26 @@
+from typing import ClassVar
+
 class Vector2:
+
+    # Type hinting for instance variables
+    x: int | float
+    y: int | float
+
+    # Type hinting for static class variables
+    up: ClassVar[Vector2]
+    down: ClassVar[Vector2]
+    left: ClassVar[Vector2]
+    right: ClassVar[Vector2]
+
+
     
-    def __init__(self, x: float, y: float):
+    def __init__(self, x: int | float, y: int | float) -> None:
         self.x = x
         self.y = y
 
 
     # Overides standard "*" tuple multiplication
-    def __mul__(self, scalar):
+    def __mul__(self, scalar: int | float) -> Vector2:
         if isinstance(scalar, (int, float)):
             # Return new Vector2 with multiplied components
             return Vector2(self.x * scalar, self.y * scalar)
@@ -15,12 +29,12 @@ class Vector2:
 
 
     # Handles reverse multiplication
-    def __rmul__(self, scalar):
+    def __rmul__(self, scalar: int | float) -> Vector2:
         return self.__mul__(scalar)
 
 
     # Handles how Vector2 is outputted
-    def __repr__(self):
+    def __repr__(self) -> str:
         return f"Vector2({self.x}, {self.y})"
 
 
