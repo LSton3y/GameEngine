a1a0bf8fd3b5f0520faa85f86b972f6f2c0332ff
LSton3y <leolstone@icloud.com>
2026-07-05 16:38:21 +0100

    feat: Add AssetManager class and managers folder

---
+++ engine/managers/asset_manager.py
@@ -0,0 +1,20 @@
+import pygame
+
+class AssetManager:
+
+    def __init__(self):
+        self._images = {} # Caches loaded images (path -> Surface)
+        self._sounds = {} # Caches loaded sounds (path -> Sound)
+    
+
+    # Loads and caches image
+    def load_image(self, image_path: str) -> pygame.Surface:
+        if image_path not in self._images:
+            self._images[image_path] = pygame.image.load(image_path).convert_alpha()
+        
+        return self._images[image_path]
+
+
+    # Loads and caches sounds
+    def load_sound(self, sound_path: str) -> pygame.mixer.Sound:
+        pass
