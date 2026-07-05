394c50c5473e1deec14e2926f19d7b016701f8fd
LSton3y <leolstone@icloud.com>
2026-07-05 17:22:56 +0100

    refactor: Emptied main.py file

--- main.py
+++ main.py
@@ -1,14 +1,2 @@
-from engine.components.transform import Transform
-from engine.components.sprite import Sprite
-from engine.rendering.renderer import Renderer
-
-
 if __name__ == "__main__":
-    ren = Renderer(500, 500)
-    tra = Transform(100, 100)
-    spr = Sprite()
-
-    
-    ren.clear()
-    ren.draw_sprite(tra, spr)
-    ren.present()
+    pass
