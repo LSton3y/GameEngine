8cd80116f7150e81b68da4916bac211ccc10abc4
LSton3y <leolstone@icloud.com>
2026-07-05 15:52:54 +0100

    feat: Add basic rendering

--- engine/rendering/renderer.py
+++ engine/rendering/renderer.py
@@ -8,6 +8,20 @@ class Renderer:
     def __init__(self, width, height):
         self.screen = pygame.display.set_mode((width, height))
 
+    
+    def clear(self):
+        self.screen.fill((255, 255, 255))
 
-def load_sprite_image(self, image_path):
+    
+    def draw_sprite(self, transform, sprite):
+        rotated = pygame.transform.rotate(sprite.image, -transform.rotation)
+        rect = rotated.get_rect(center=transform.position.to_tuple())
+        self.screen.blit(rotated, rect)
+    
+    def present(self):
+        pygame.display.flip()
+
+
+def load_sprite_image(image_path):
     return pygame.image.load(image_path).convert_alpha()
+
--- main.py
+++ main.py
@@ -1,5 +1,14 @@
-from engine.math.vector2 import Vector2
+from engine.components.transform import Transform
+from engine.components.sprite import Sprite
+from engine.rendering.renderer import Renderer
 
 
 if __name__ == "__main__":
-    print(Vector2(5, 12).normalize())
+    ren = Renderer(500, 500)
+    tra = Transform(100, 100)
+    spr = Sprite()
+
+    
+    ren.clear()
+    ren.draw_sprite(tra, spr)
+    ren.present()
