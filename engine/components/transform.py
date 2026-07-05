0ec1fb038f6a9c491d044352dbcd1fdbbd800961
LSton3y <leolstone@icloud.com>
2026-07-05 16:06:54 +0100

    style: Add class documentation for transform class

--- engine/components/transform.py
+++ engine/components/transform.py
@@ -1,16 +1,16 @@
-# Transform class
-
-# Stores Vector2 position and scale
-# Stores rotation as a float
-
-# Handles appearance and location of an entity
-
 from engine.math.vector2 import Vector2
 
 
 class Transform:
+    """
+    Stores position, rotation and scale of an entity
+    Attributes:
+        position (Vector2): Vector2 representation of entity position
+        rotation (int | float): Rotation of entity
+        scale (Vector2): Vector2 representation of entity scale
+    """
     
-    def __init__(self, x, y) -> None:
-        self.position = Vector2(x, y)
-        self.rotation = 0
-        self.scale = Vector2(0, 0)
+    def __init__(self, position: Vector2 = Vector2(0, 0), rotation: int | float = 0, scale: Vector2 = Vector2(0, 0)) -> None:
+        self.position = position
+        self.rotation = rotation
+        self.scale = scale
