10761b409a0d8bc14452cf516e69ae36ca18c130
LSton3y <leolstone@icloud.com>
2026-07-05 21:54:06 +0100

    feat: Add enity class with component methods

---
+++ engine/core/entity.py
@@ -0,0 +1,26 @@
+class Entity:
+    """
+    Base object in the game engine.\n
+    Can store components like Transform, Sprite, etc.
+    """
+
+    def __init__(self):
+        self.components = {} # type -> component reference
+
+    
+    # Adds component to entity
+    def add_component(self, component) -> dict:
+        self.components[type(component)] = component
+        return component # Allows for chaining, e.g. entity.add_component(Transform).x
+
+    # Removes component from entity
+    def remove_component(self, component_type) -> None:
+        self.components.pop(component_type, None)
+    
+    # Gets component from entity
+    def get_component(self, component_type):
+        return self.components.get(component_type)
+
+    # Checks if component is in entity
+    def has_component(self, component_type):
+        return component_type in self.components
