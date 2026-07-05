b7929a631dd22a5dadbacac607443b41b5c6ffab
LSton3y <leolstone@icloud.com>
2026-07-05 22:19:10 +0100

    style: Add comments to methods

--- engine/rendering/renderer.py
+++ engine/rendering/renderer.py
@@ -10,15 +10,19 @@ class Renderer:
         self.window = window
 
     
+    # Fills screen with color
     def clear(self, color=(255, 255, 255)):
         self.window.fill(color)
 
     
+    # Draws sprite
     def draw_sprite(self, transform, sprite):
         rotated = pygame.transform.rotate(sprite.image, -transform.rotation)
         rect = rotated.get_rect(center=transform.position.to_tuple())
         self.window.blit(rotated, rect)
     
+
+    # Updates display
     def update(self):
         pygame.display.flip()
 
