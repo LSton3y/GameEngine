81083d0b65d09ef103e38d06b0198210ab62f81f
LSton3y <leolstone@icloud.com>
2026-07-05 16:11:21 +0100

    style: Add class documentation for Sprite class

--- engine/components/sprite.py
+++ engine/components/sprite.py
@@ -1,8 +1,12 @@
-# Sprite class
 from engine.rendering.renderer import load_sprite_image
 
 
 class Sprite:
+    """
+    Handles the visual representation of an entity
+    Attributes:
+        image (pygame.Surface): Pygame surface rendering sprite image
+    """
 
     def __init__(self, image_path="assets/sprites/scratch-cat.png"):
         self.image = load_sprite_image(image_path)
