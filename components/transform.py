398493d79807bdf93e43772014ec4dfbf58fb665
LSton3y <leolstone@icloud.com>
2026-07-05 14:06:01 +0100

    feat: Add transform class

--- components/transform.py
+++ components/transform.py
@@ -1,2 +1,17 @@
+# Transform class
+
+# Stores Vector2 position and scale
+# Stores rotation as a float
+
+# Handles appearance and location of an entity
+
+from components.vector2 import Vector2
+
+
+
 class Transform:
-    pass
+    
+    def __init__(self) -> None:
+        self.position = Vector2(0, 0)
+        self.rotation = 0
+        self.scale = Vector2(0, 0)
