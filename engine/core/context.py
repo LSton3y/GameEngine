0a66a5b1f8fc7a3e32325d5c5e20387e6667ef77
LSton3y <leolstone@icloud.com>
2026-07-05 16:49:21 +0100

    style: Add type hinting for Context class

--- engine/core/context.py
+++ engine/core/context.py
@@ -1,11 +1,22 @@
+from engine.rendering.window import Window
+from engine.rendering.renderer import Renderer
+from engine.managers.asset_manager import AssetManager
+from engine.managers.input_manager import InputManager
+
+
 class Context:
     """
     Central service locator. Holds references to engine-wide singletons
     (managers) so systems/scenes don't need long constructor argument lists.
+    Attributes:
+        window (Window): Window class
+        renderer (Renderer): Renderer class
+        assets (AssetManager): AssetManager class
+        input (InputManager): InputManager class
     """
 
     def __init__(self, window, renderer, asset_manager, input_manager):
-        self.window = window
-        self.renderer = renderer
-        self.asset_manager = asset_manager
-        self.input = input_manager
+        self.window: Window = window
+        self.renderer: Renderer = renderer
+        self.assets: AssetManager = asset_manager
+        self.input: InputManager = input_manager
