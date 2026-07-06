896e49e45542f4d5b9d43046ad630cb5a1ded401
LSton3y <leolstone@icloud.com>
2026-07-06 21:19:18 +0100

    style: Add comments to SingletonMeta class

--- engine/core/singleton.py
+++ engine/core/singleton.py
@@ -1,11 +1,21 @@
 class SingletonMeta(type):
-    _instances = {}
+    """
+    Metaclass used to create singleton classes
+    
+    Use instance() to call class instance
+    """
 
+    _instances = {} # Stores singleton instances
+
+
+    # Called every time the class is called, e.g. Class()
     def __call__(cls, *args, **kwargs):
         if cls not in cls._instances:
             cls._instances[cls] = super().__call__(*args, **kwargs)
         return cls._instances[cls]
 
+
+    # Returns class instance
     def instance(cls):
         if cls not in cls._instances:
             raise RuntimeError(f"{cls.__name__} has not been created yet")
