dfce66c3831cf968d89ec7a31476a93e0a4cb9e5
LSton3y <leolstone@icloud.com>
2026-07-06 21:35:30 +0100

    fix: Changed components in Entity class to private

--- engine/core/entity.py
+++ engine/core/entity.py
@@ -6,22 +6,22 @@ class Entity:
     """
 
     def __init__(self):
-        self.components = {} # type -> component reference
+        self._components = {} # type -> component reference
 
     
     # Adds component to entity
     def add_component(self, component) -> dict:
-        self.components[type(component)] = component
+        self._components[type(component)] = component
         return component # Allows for chaining, e.g. entity.add_component(Transform).x
 
     # Removes component from entity
     def remove_component(self, component_type) -> None:
-        self.components.pop(component_type, None)
+        self._components.pop(component_type, None)
     
     # Gets component from entity
     def get_component(self, component_type):
-        return self.components.get(component_type)
+        return self._components.get(component_type)
 
     # Checks if component is in entity
     def has_component(self, component_type):
-        return component_type in self.components
+        return component_type in self._components
