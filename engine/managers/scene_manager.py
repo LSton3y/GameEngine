454cd6dafe11e2e0329cfd80d6207b98e929ae55
LSton3y <leolstone@icloud.com>
2026-07-07 18:38:43 +0100

    feat: Add SceneManager class

---
+++ engine/managers/scene_manager.py
@@ -0,0 +1,29 @@
+from engine.core.scene import Scene
+from engine.core.singleton import SingletonMeta
+
+
+class SceneManager(metaclass=SingletonMeta):
+    """
+    Stores all the scenes
+
+    Controls which scenes are currently active
+    """
+
+    def __init__(self):
+        # Stores all the scenes
+        self._scenes = [Scene()]
+        # Stores the current scene
+        self._current_scene = self._scenes[0]
+    
+
+    def change_scene_index(self, index) -> None:
+        # If scene index exists, change active scene
+        if index <= len(self._scenes):
+            self._current_scene = self._scenes[index]
+        else:
+            return IndexError
+    
+
+    @property
+    def current_scene(self):
+        return self._current_scene
