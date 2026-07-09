ba6314efdad2992952d12bacab303de69b676148
LSton3y <leolstone@icloud.com>
2026-07-09 19:57:26 +0100

    feat: Added imports and transform property

--- engine/components/script.py
+++ engine/components/script.py
@@ -1,4 +1,14 @@
+from engine.systems.input_system import InputSystem
+from engine.managers.input_manager import InputManager
+
+from engine.components.transform import Transform
+from engine.math.vector2 import Vector2
+
+
 class Script:
+    def __init__(self, entity):
+        self.entity = entity
+
 
     def start(self):
         """Called the first frame the script runs."""
@@ -8,3 +18,8 @@ class Script:
     def update(self, dt: float):
         """Called every frame."""
         pass
+
+    
+    @property
+    def transform(self):
+        self.entity.get_component(Transform)
