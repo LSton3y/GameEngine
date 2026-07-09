469716775377b67e1c49b24512e2bc558f479112
LSton3y <leolstone@icloud.com>
2026-07-09 18:27:57 +0100

    feat: Added update method for RenderSystem class

--- engine/systems/render_system.py
+++ engine/systems/render_system.py
@@ -1,4 +1,18 @@
+from engine.components.sprite import Sprite
+from engine.components.sprite import Transform
+
 from engine.systems.base_system import BaseSystem
+from engine.managers.scene_manager import SceneManager
+
+from engine.rendering.renderer import Renderer
+
 
 class RenderSystem(BaseSystem):
-    pass
+
+    def update(self, dt):
+        for entity in SceneManager.instance().entities:
+            if entity.has_component(Sprite) and entity.has_component(Transform):
+                Renderer.instance().draw_sprite(
+                    entity.get_component(Transform),
+                    entity.get_component(Sprite)
+                )
