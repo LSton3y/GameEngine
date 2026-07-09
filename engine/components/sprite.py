6a2d5855bcb79bbe3179f33f380d53d5d08d6fb4
LSton3y <leolstone@icloud.com>
2026-07-09 23:13:21 +0100

    refactor: Changed Sprite dict conversion methods

--- engine/components/sprite.py
+++ engine/components/sprite.py
@@ -1,29 +1,23 @@
 from engine.managers.asset_manager import AssetManager
+from engine.serialization.serializable import Serializable
 
 
-class Sprite:
+class Sprite(Serializable):
     """
     Handles the visual representation of an entity
     Attributes:
         image_path (str): Stores the image path
         image (pygame.Surface): Pygame surface rendering sprite image
     """
+    _exclude = {"image"}
+
 
     def __init__(self, image_path: str="game/assets/sprites/scratch-cat.png"):
         self.image_path = image_path
         self.image = AssetManager.instance().load_image(image_path)
     
 
-    # Converts sprite properties to dict
-    def to_dict(self):
-        return {
-            "image_path": self.image_path
-        }
-    
-
     # Returns class created from dict properties
     @classmethod
     def from_dict(cls, data):
-        return cls(
-            image_path=data["image_path"]
-        )
+        return cls(**data)
