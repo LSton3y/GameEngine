44897de6225589985387211d30575033fe1c196a
LSton3y <leolstone@icloud.com>
2026-07-09 22:20:28 +0100

    feat: Added saving and loading to SceneManager class

--- engine/managers/scene_manager.py
+++ engine/managers/scene_manager.py
@@ -1,3 +1,5 @@
+import json
+
 from engine.core.scene import Scene
 from engine.core.singleton import SingletonMeta
 
@@ -11,9 +13,22 @@ class SceneManager(metaclass=SingletonMeta):
 
     def __init__(self):
         # Stores all the scenes
-        self._scenes = [Scene()]
+        self._scenes = []
         # Stores the current scene
-        self._current_scene = self._scenes[0]
+        self._current_scene = None
+
+    
+    # Saves current scene as a json file
+    def save(self, path):
+        with open(path, "w") as file:
+            json.dump(self._current_scene.to_dict(), file, indent=2)
+    
+    # Loads current scene from json file
+    def load(self, path, registry):
+        with open(path) as file:
+            data = json.load(file)
+        
+        self._current_scene = Scene.from_dict(data, registry)
     
 
     def change_scene_index(self, index) -> None:
