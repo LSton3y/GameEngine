fd791f15010de2302bb7887b5c06b5bc887b6bb0
LSton3y <leolstone@icloud.com>
2026-07-09 23:09:24 +0100

    refactor: Cleaned Serializable class code

--- engine/serialization/serializable.py
+++ engine/serialization/serializable.py
@@ -1,20 +1,32 @@
 class Serializable:
-    # Attributes which should be excluded from being serialized
+    """
+    Base class for objects that can be serialized to dictionaries.
+    """
+
+    # Attributes that shouldn't be saved
     _exclude = set()
 
 
-    # Converts class properties to dict
     def to_dict(self):
-        return {
-            key: value.to_dict() if hasattr(value, "to_dict") else value
-            for key, value in self.__dict__.items()
-            if key not in self._exclude
-        }
+        data = {}
 
-    
-    # Returns class created from data properties
+        for key, value in self.__dict__.items():
+            if key in self._exclude:
+                continue
+
+            if hasattr(value, "to_dict"):
+                data[key] = value.to_dict()
+            else:
+                data[key] = value
+
+        return data
+
+
     @classmethod
     def from_dict(cls, data):
         obj = cls.__new__(cls)
-        obj.__data__.update(data)
+
+        for key, value in data.items():
+            setattr(obj, key, value)
+
         return obj
