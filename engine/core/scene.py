383377f91c5bdf522ace2f01a9c4e1185059393b
LSton3y <leolstone@icloud.com>
2026-07-09 20:04:40 +0100

    test: Added test script to sprite in scene

--- engine/core/scene.py
+++ engine/core/scene.py
@@ -2,6 +2,7 @@ from engine.core.entity import Entity
 from engine.components.transform import Transform
 from engine.components.sprite import Sprite
 from engine.math.vector2 import Vector2
+from game.scripts.player_movement import PlayerMovement
 
 
 class Scene:
@@ -16,6 +17,7 @@ class Scene:
         sprite = Entity()
         sprite.add_component(Transform(Vector2(150, 200)))
         sprite.add_component(Sprite())
+        sprite.add_component(PlayerMovement())
 
         self._entities = [sprite]
     
