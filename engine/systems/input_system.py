35786c09114977d535d947c5005f31ce2e4df004
LSton3y <leolstone@icloud.com>
2026-07-09 19:30:59 +0100

    feat: Added actions to InputSystem class

--- engine/systems/input_system.py
+++ engine/systems/input_system.py
@@ -1,4 +1,37 @@
-from engine.systems.base_system import BaseSystem
+import pygame
 
-class InputSystem(BaseSystem):
-    pass
+from engine.systems.base_system import BaseSystem
+from engine.managers.input_manager import InputManager
+from engine.core.singleton import SingletonMeta
+
+
+class InputSystem(BaseSystem, metaclass=SingletonMeta):
+    """
+    Handles input actions
+    """
+
+    def __init__(self):
+        self._action_map = {
+            "move_up": [pygame.K_w, pygame.K_UP],
+            "move_down": [pygame.K_s, pygame.K_DOWN],
+            "move_left": [pygame.K_a, pygame.K_LEFT],
+            "move_right": [pygame.K_d, pygame.K_RIGHT],
+        }
+
+    # Checks if action is down
+    def is_action_down(self, action: str) -> bool:
+        keys = self._action_map.get(action, [])
+        input_mgr = InputManager.instance()
+        return any(input_mgr.is_key_down(k) for k in keys)
+
+    # Checks if action is pressed
+    def is_action_pressed(self, action: str) -> bool:
+        keys = self._action_map.get(action, [])
+        input_mgr = InputManager.instance()
+        return any(input_mgr.is_key_pressed(k) for k in keys)
+
+    # Checks if action is released
+    def is_action_pressed(self, action: str) -> bool:
+        keys = self._action_map.get(action, [])
+        input_mgr = InputManager.instance()
+        return any(input_mgr.is_key_pressed(k) for k in keys)
