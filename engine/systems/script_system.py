605a10c0b4bf631f4c27487f6f29aae861cad572
LSton3y <leolstone@icloud.com>
2026-07-09 21:05:28 +0100

    fix: Added start method to ScriptSystem class

--- engine/systems/script_system.py
+++ engine/systems/script_system.py
@@ -8,7 +8,15 @@ class ScriptSystem(BaseSystem):
     Handles the execution of scripts attached to entities
     """
 
+    def start(self):
+        for entity in SceneManager.instance().current_scene.entities:
+            for component in entity.get_components():
+                if isinstance(component, Script):
+                    component.start()
+                    
+
     def update(self, dt):
         for entity in SceneManager.instance().current_scene.entities:
-            if entity.has_component(Script):
-                entity.get_component(Script).update(dt)
+            for component in entity.get_components():
+                if isinstance(component, Script):
+                    component.update(dt)
