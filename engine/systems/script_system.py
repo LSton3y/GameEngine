1637e9e144faa1194831ff409c2f1512c4f866ef
LSton3y <leolstone@icloud.com>
2026-07-09 19:35:43 +0100

    feat: Added update method for ScriptSystem class

--- engine/systems/script_system.py
+++ engine/systems/script_system.py
@@ -1,6 +1,14 @@
 from engine.systems.base_system import BaseSystem
 from engine.managers.scene_manager import SceneManager
+from engine.components.script import Script
 
 
 class ScriptSystem(BaseSystem):
-    pass
+    """
+    Handles the execution of scripts attached to entities
+    """
+
+    def update(self, dt):
+        for entity in SceneManager.instance().current_scene.entities:
+            if entity.has_component(Script):
+                entity.get_component(Script).update(dt)
