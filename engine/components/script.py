01dbc04d23d07e8da629655606a1c8da5cfa3f1a
LSton3y <leolstone@icloud.com>
2026-07-09 22:35:50 +0100

    feat: Added dict conversion methods for Script class

--- engine/components/script.py
+++ engine/components/script.py
@@ -4,6 +4,10 @@ from engine.components.transform import Transform
 
 
 class Script:
+    # Attributes which shouldn't be serialized
+    _exclude_from_dict = {"entity"}
+
+
     def __init__(self, entity):
         self.entity = entity
 
@@ -16,6 +20,25 @@ class Script:
     def update(self, dt: float):
         """Called every frame."""
         pass
+
+    
+    # Serialization
+    def to_dict(self):
+        data = {}
+        for key, value in self.__dict__.items():
+            if key in self._exclude_from_dict:
+                continue
+            # Handle nested objects that know how to serialize themselves (e.g. Vector2)
+            data[key] = value.to_dict() if hasattr(value, "to_dict") else value
+
+        return data
+
+    @classmethod
+    def from_dict(cls, data, entity):
+        instance = cls(entity)
+        instance.__dict__.update(data)
+
+        return instance
 
     
     @property
