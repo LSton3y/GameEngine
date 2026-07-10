4f84833f9451f024ee677e56c3b565e5682c896a
LSton3y <leolstone@icloud.com>
2026-07-10 18:16:15 +0100

    refactor: Changed to_dict method in Serializable class

--- engine/serialization/serializable.py
+++ engine/serialization/serializable.py
@@ -7,18 +7,27 @@ class Serializable:
     _exclude = set()
 
 
+    # Serializes a value into a dict
+    def serialize(self, value):
+        if hasattr(value, "to_dict"):
+            return value.to_dict()
+
+        if isinstance(value, list):
+            return [self.serialize(v) for v in value]
+
+        if isinstance(value, dict):
+            return {
+                k.__name__: self.serialize(v)
+                for k, v in value.items()
+            }
+
+        return value
+
+
     def to_dict(self):
-        data = {}
-
-        for key, value in self.__dict__.items():
-            if key in self._exclude:
-                continue
-
-            if hasattr(value, "to_dict"):
-                data[key] = value.to_dict()
-            else:
-                data[key] = value
-
-        return data
-    
+        return {
+            k: self.serialize(v)
+            for k, v in self.__dict__.items()
+            if k not in self._exclude
+        }
             
