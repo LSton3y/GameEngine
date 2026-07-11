f9532b6f9be4f0e7ff661c9cddc025c59a7b8ff8
LSton3y <leolstone@icloud.com>
2026-07-11 10:29:11 +0100

    refactor: Changed how Singleton class works

--- engine/core/singleton.py
+++ engine/core/singleton.py
@@ -7,12 +7,14 @@ class Singleton:
     _instance = None
 
 
-    def __init__(self, cls):
-        if cls._instance is not None:
-            raise ValueError("Class instance already exists.")
-        else:
-            cls._instance = self
-  
+    def __new__(cls, *args, **kwargs):
+        if cls._instance is None:
+            cls._instance = super().__new__(cls)
+        return cls._instance
+
+
     @classmethod
-    def get_instance(cls):
+    def instance(cls):
+        if cls._instance is None:
+          raise ValueError(f"{cls.__name__} singleton does not exist.")
         return cls._instance
