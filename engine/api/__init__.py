61b3b02e7a8d9d358f664a2651f82dc5879c8e1c
LSton3y <leolstone@icloud.com>
2026-07-13 21:04:19 +0100

    fix: Fixed api imports

--- engine/api/__init__.py
+++ engine/api/__init__.py
@@ -1,3 +1,3 @@
-import input_api as input
-import entity_api as entity
-import scene_api as scene
+import engine.api.input_api as input
+import engine.api.entity_api as entity
+import engine.api.scene_api as scene
